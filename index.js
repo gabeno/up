d89d85bc971891103d688e80127401e9d81b8522
Gabriel Majivu <gmajivu@gmail.com>
2018-04-30 08:24:53 +0300

    parse http method

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
     const path = parsedUrl.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase();
 
     res.end('Hello world\n');
 
-    console.log(`Request received on path: ${trimmedPath}`);
+    console.log(`Request received on path: ${method} ${trimmedPath}`);
 });
 
 server.listen(3000, () => {
