b08165e9acc19643789ee6afb803542f4a5dd684
Gabriel Majivu <gabriel@medicmobile.org>
2018-04-30 14:55:42 +0300

    parse payload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer((req, res) => {
     const parsedUrl = url.parse(req.url, true);
@@ -11,12 +12,25 @@ const server = http.createServer((req, res) => {
     const method = req.method.toLowerCase();
     const headers = req.headers;
 
-    res.end('Hello world\n');
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
 
-    // curl "localhost:3000/foo/bar/?q=fizz" -H "foo: bar"
-    console.log(`Request received on path: ${method} ${trimmedPath}`);
-    console.log(`Query Object: ${JSON.stringify(queryObj)}`);
-    console.log(`Headers: ${JSON.stringify(headers)}`);
+    req.on('data', (data) => {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+
+        res.end('Hello world\n');
+
+        // curl "localhost:3000/foo/bar/?q=fizz" -H "foo: bar"
+        // curl -XPOST "localhost:3000/foo/bar/?q=name" -H "foo: bar" --data-raw "This is a sample body content"
+        console.log(`Request received on path: ${method} ${trimmedPath}`);
+        console.log(`Query Object: ${JSON.stringify(queryObj)}`);
+        console.log(`Headers: ${JSON.stringify(headers)}`);
+        console.log(`Payload: ${buffer}`);
+    });
 });
 
 server.listen(3000, () => {
