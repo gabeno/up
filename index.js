5c087c25fe951bfcc3162e1226354694cf8c5442
Gabriel Majivu <gabriel@medicmobile.org>
2018-05-14 13:53:50 +0300

    refactor and organize sever files

--- index.js
+++ index.js
@@ -1,110 +1,18 @@
-const http = require("http");
-const https = require("https");
-const url = require("url");
-const StringDecoder = require("string_decoder").StringDecoder;
-const fs = require('fs');
+const server = require('./lib/server');
+const workers  = require('./lib/workers');
 
-const config = require('./config');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+// App
+const app = {};
 
-// @TODO: write TDD 
-/*
-const _data = require('./lib/data');
-_data.delete('test', 'new_file', (err, data) => {
-  console.log(err);
-  // console.log(data);
-});
-*/
+// Init function
+app.init = () => {
+  // Start the server
+  server.init();
 
-// Instantiate HTTP server
-const httpSserver = http.createServer((req, res) => {
-  unifiedServer(req, res);
-});
-
-// start HTTP server
-httpSserver.listen(config.httpPort, () => {
-  console.log(`The server is listening on port ${config.httpPort} in ${config.envName} mode`);
-});
-
-// Instantiate HTTPS server
-const httpsServerOpts = {
-  'key': fs.readFileSync('./https/key.pem', 'utf-8'),
-  'cert': fs.readFileSync('./https/cert.pem', 'utf-8')
-};
-const httpsSserver = https.createServer(httpsServerOpts, (req, res) => {
-  unifiedServer(req, res);
-});
-
-// start HTTPS server
-httpsSserver.listen(config.httpsPort, () => {
-  console.log(`The server is listening on port ${config.httpsPort} in ${config.envName} mode`);
-});
-
-/**
- * Handle server logic - usable by http(s)
- * @param {*} req - request
- * @param {*} res - response
- */
-const unifiedServer = (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
-  const queryObj = parsedUrl.query;
-
-  const method = req.method.toLowerCase();
-  const headers = req.headers;
-
-  const decoder = new StringDecoder("utf-8");
-  let buffer = "";
-
-  req.on("data", data => {
-    buffer += decoder.write(data);
-  });
-
-  req.on("end", () => {
-    buffer += decoder.end();
-
-    // choose handler
-    const chosenHandler =
-      typeof router[trimmedPath] !== "undefined"
-        ? router[trimmedPath]
-        : handlers.notFound;
-
-    // collate data
-    const data = {
-      path: trimmedPath,
-      method: method,
-      payload: helpers.parseJsonToObject(buffer),
-      headers: headers,
-      queryStringObject: queryObj
-    };
-
-    // route request
-    chosenHandler(data, (status, load) => {
-      const statusCode = typeof status == "number" ? status : 200;
-      const payload = typeof load == "object" ? load : {};
-      const payloadString = JSON.stringify(payload);
-
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      console.log(`Request received on path: ${method} ${trimmedPath}`);
-      console.log(`Query Object: ${JSON.stringify(queryObj)}`);
-      console.log(`Headers: ${JSON.stringify(headers)}`);
-      console.log(`Payload: ${buffer}`);
-      console.log(`Response: ${statusCode} ${payloadString}`);
-    });
-  });
+  // Start the workers
+  // workers.init();
 };
 
-const router = {
-  'ping': handlers.ping,
-  'users': handlers.users,
-  'tokens': handlers.tokens
-};
+app.init();
 
-// curl "localhost:3000/foo/bar/?q=fizz" -H "foo: bar"
-// curl -XPOST "localhost:3000/foo/bar/?q=name" -H "foo: bar" --data-raw "This is a sample body content"
+module.exports = app;
